6a046ca7df111f06671d525ba8355240e6266302
tomasrcoelho <tomasrcoelho@gmail.com>
2024-12-19 10:59:43 +0000

    day 18 complete

--- day-16/main.ts
+++ day-16/main.ts
@@ -6,11 +6,7 @@ enum Direction {
   VERTICAL = 1,
 }
 
-type Possibility = [
-  visited: [number, number][],
-  points: number,
-  direction: number,
-];
+type Possibility = [visited: [number, number, number][], direction: number];
 
 const dirs = [
   [1, 0],
@@ -22,72 +18,116 @@ const dirs = [
 let init_dir = Direction.HORIZONTAL;
 const finished_paths: number[] = [];
 let paths: Possibility[] = [];
+let newPaths: Possibility[] = [];
 
 async function main() {
   const grid = await readfile();
   const [init_x, init_y] = [1, grid[0].length - 2];
-  paths.push([[[init_x, init_y]], 0, init_dir]);
+  paths.push([[[init_x, init_y, 0]], init_dir]);
   while (paths.length !== 0) {
-    const newPaths: Possibility[] = [];
+    newPaths = [];
     for (const path of paths) {
       const recent = nextpaths(path, grid);
       recent.length > 0 && newPaths.push(...recent);
     }
-    paths = newPaths.sort((a, b) => a[1] - b[1]);
+
+    paths = newPaths;
   }
-  console.log([...new Set(finished_paths)].sort((a, b) => a - b)[0]);
+  console.log([...new Set(finished_paths)].sort((a, b) => a - b));
 }
 
-const isVisited = (arr: [number, number][], x: number, y: number) =>
+const isVisited = (arr: [number, number, number][], x: number, y: number) =>
   arr.some((el) => el[0] === x && el[1] === y);
 
 function nextpaths(path: Possibility, grid: string[]): Possibility[] {
-  const newPaths: Possibility[] = [];
-  const [visited, points, direction] = path;
+  const currNewPaths: Possibility[] = [];
+  const [visited, direction] = path;
   const [curr_x, curr_y] = visited[visited.length - 1];
 
   for (const [x_dir, y_dir] of dirs) {
     const newDirection =
       x_dir === 0 ? Direction.VERTICAL : Direction.HORIZONTAL;
-    const newPoints = points + (direction === newDirection ? 1 : 1001);
-    if (newPoints > 105576) continue;
+
+    const newPoints = direction === newDirection ? 1 : 1001;
+
     //Check if next position is the final
     if (grid[curr_y + y_dir][curr_x + x_dir] === END_TILE) {
-      finished_paths.push(newPoints);
+      finished_paths.push(getPositionCost(visited) + newPoints);
       continue;
     }
 
     // Check if possible to follow new path
     if (
+      grid[curr_y + y_dir][curr_x + x_dir] === EMPTY_TILE &&
       !isVisited(visited, curr_x + x_dir, curr_y + y_dir) &&
-      grid[curr_y + y_dir][curr_x + x_dir] === EMPTY_TILE
-    ) {
-      newPaths.push([
-        [...visited, [curr_x + x_dir, curr_y + y_dir]],
+      isShortestPath(
+        curr_x + x_dir,
+        curr_y + y_dir,
+        visited,
         newPoints,
+        newDirection,
+      )
+    ) {
+      currNewPaths.push([
+        [...visited, [curr_x + x_dir, curr_y + y_dir, newPoints]],
         newDirection,
       ]);
     }
   }
 
-  return newPaths;
+  return currNewPaths;
+}
+
+function getPositionCost(arr: [number, number, number][]) {
+  return arr.reduce((prev, curr) => curr[2] + prev, 0);
 }
 
 function isShortestPath(
   x: number,
   y: number,
-  currPath: [number, number][],
+  currPath: [number, number, number][],
   points: number,
+  newDirection: number,
 ): Boolean {
-  const index = paths.findIndex(
-    ([visited, pts, dir]) => isVisited(visited, x, y) && points > pts,
-  );
-  if (index > -1) return false;
-  return true;
+  let pos: number = 0;
+  let isShorter: Boolean = false;
+  const index = paths.findIndex(([visited, dir], i) => {
+    if (isVisited(visited, x, y)) {
+      pos = i;
+      return true;
+    }
+    return false;
+  });
+  // let posNew: number = 0;
+  // const indexNew = newPaths.findIndex(([visited, dir], i) => {
+  //   if (isVisited(visited, x, y)) {
+  //     posNew = i;
+  //     return true;
+  //   }
+  //   return false;
+  // });
+  if (index === -1) return true; //&& indexNew === -1
+  if (
+    index !== -1 &&
+    getPositionCost(paths[index][0].slice(0, pos + 1)) >=
+      getPositionCost(currPath) + points
+  ) {
+    paths.splice(index, 1);
+    isShorter = true;
+  }
+  // if (
+  //   indexNew !== -1 &&
+  //   getPositionCost(newPaths[indexNew][0].slice(0, posNew + 1)) >=
+  //     getPositionCost(currPath) + points
+  // ) {
+  //   newPaths.splice(indexNew, 1);
+  //   isShorter = true;
+  // }
+  return isShorter;
 }
 
 async function readfile() {
-  const file = Bun.file("test.txt");
+  const file = Bun.file("input.txt");
   return (await file.text()).split("\n");
 }
 
--- day-18/main.ts
+++ day-18/main.ts
@@ -1,11 +1,11 @@
-const CORRUPTED_BYTES_COUNT = 12;
-const MEMORY_SIDE_SIZE = 6;
+const CORRUPTED_BYTES_COUNT = 1024;
+const MEMORY_SIDE_SIZE = 70;
 const EMPTY_TILE = ".";
 const END_TILE = "E";
 
 const grid: string[] = [];
 
-type Possibility = [visited: [number, number][], points: number];
+type Possibility = [visited: [number, number, number][]];
 
 const dirs = [
   [1, 0],
@@ -13,8 +13,16 @@ const dirs = [
   [0, -1],
   [0, 1],
 ];
+const diagonals = [
+  [1, 1],
+  [-1, -1],
+  [1, -1],
+  [-1, 1],
+];
 
 let finished = 0;
+let paths: Possibility[] = [];
+let newPaths: Possibility[] = [];
 
 for (let i = 0; i <= MEMORY_SIDE_SIZE; i++)
   grid.push(".".repeat(MEMORY_SIDE_SIZE + 1));
@@ -23,61 +31,136 @@ async function main() {
   const corrupted_bytes = await readfile();
   for (let i = 0; i < CORRUPTED_BYTES_COUNT; i++) {
     const [x, y] = corrupted_bytes[i].split(",");
-    const newLine = grid[y].split("");
-    newLine[x] = "#";
-    grid[y] = newLine.join("");
+    changeMap(x, y, "#");
   }
-  const newLine = grid[MEMORY_SIDE_SIZE].split("");
-  newLine[MEMORY_SIDE_SIZE] = "E";
-  grid[MEMORY_SIDE_SIZE] = newLine.join("");
-  console.log(grid.join("\n"));
+  changeMap(String(MEMORY_SIDE_SIZE), String(MEMORY_SIDE_SIZE), "E");
 
-  let paths: Possibility[] = [[[[0, 0]], 0]];
-  while (paths.length !== 0) {
-    const newPaths: Possibility[] = [];
-    for (const path of paths) {
-      const recent = nextpaths(path, grid);
-      recent.length > 0 && newPaths.push(...recent);
+  for (let i = CORRUPTED_BYTES_COUNT + 1; i < corrupted_bytes.length; i++) {
+    const [x, y] = corrupted_bytes[i].split(",");
+    changeMap(x, y, "#");
+    finished = 0;
+    paths = [];
+    paths.push([[[0, 0, 0]]]);
+
+    let isIsolated = true;
+    for (const [dir_x, dir_y] of dirs) {
+      if (grid[+y + dir_y]?.[+x + dir_x] === "#") {
+        isIsolated = false;
+        break;
+      }
     }
-    if (finished !== 0) break;
-    paths = newPaths;
+
+    for (const [dir_x, dir_y] of diagonals) {
+      if (grid[+y + dir_y]?.[+x + dir_x] === "#") {
+        isIsolated = false;
+        break;
+      }
+    }
+    if (isIsolated) continue;
+
+    while (paths.length !== 0) {
+      newPaths = [];
+      for (const path of paths) {
+        const recent = nextpaths(path, grid);
+        recent.length > 0 && newPaths.push(...recent);
+      }
+      if (finished !== 0) break;
+      paths = newPaths
+        .sort((a, b) => getPositionCost(a[0]) - getPositionCost(b[0]))
+        .splice(0, 100);
+    }
+    if (finished === 0) {
+      console.log("aqui", x, y);
+      break;
+    }
   }
+
   console.log(finished);
 }
 
-const isVisited = (arr: [number, number][], x: number, y: number) =>
+const isVisited = (arr: [number, number, number][], x: number, y: number) =>
   arr.some((el) => el[0] === x && el[1] === y);
 
 function nextpaths(path: Possibility, grid: string[]): Possibility[] {
-  const newPaths: Possibility[] = [];
-  const [visited, points] = path;
-  const [curr_x, curr_y] = visited[visited.length - 1];
+  const currNewPaths: Possibility[] = [];
+  const [visited] = path;
+  const [curr_x, curr_y, ...rest] = visited[visited.length - 1];
 
   for (const [x_dir, y_dir] of dirs) {
-    const newPoints = points + 1;
     //Check if next position is the final
-    if (grid[curr_y + y_dir]?.[curr_x + x_dir] === END_TILE) {
-      finished = newPoints;
+    if (grid[+curr_y + y_dir]?.[+curr_x + x_dir] === END_TILE) {
+      finished = getPositionCost(visited) + 1;
       continue;
     }
 
     // Check if possible to follow new path
     if (
-      !isVisited(visited, curr_x + x_dir, curr_y + y_dir) &&
-      grid[curr_y + y_dir]?.[curr_x + x_dir] === EMPTY_TILE
+      grid[+curr_y + y_dir]?.[+curr_x + x_dir] === EMPTY_TILE &&
+      !isVisited(visited, +curr_x + x_dir, +curr_y + y_dir) &&
+      isShortestPath(+curr_x + x_dir, +curr_y + y_dir, visited, 1)
     ) {
-      newPaths.push([
-        [...visited, [curr_x + x_dir, curr_y + y_dir]],
-        newPoints,
-      ]);
+      currNewPaths.push([[...visited, [+curr_x + x_dir, +curr_y + y_dir, 1]]]);
     }
   }
 
-  return newPaths;
+  return currNewPaths;
+}
+
+function getPositionCost(arr: [number, number, number][]) {
+  return arr.reduce((prev, curr) => curr[2] + prev, 0);
+}
+
+function isShortestPath(
+  x: number,
+  y: number,
+  currPath: [number, number, number][],
+  points: number,
+): Boolean {
+  let pos: number = 0;
+  let isShorter: Boolean = false;
+  const index = paths.findIndex(([visited], i) => {
+    if (isVisited(visited, x, y)) {
+      pos = i;
+      return true;
+    }
+    return false;
+  });
+  let posNew: number = 0;
+  const indexNew = newPaths.findIndex(([visited], i) => {
+    if (isVisited(visited, x, y)) {
+      posNew = i;
+      return true;
+    }
+    return false;
+  });
+  if (index === -1 && indexNew === -1) return true; //&& indexNew === -1
+  if (
+    index !== -1 &&
+    getPositionCost(paths[index][0].slice(0, pos + 1)) >=
+      getPositionCost(currPath) + points
+  ) {
+    paths.splice(index, 1);
+    isShorter = true;
+  }
+  if (
+    indexNew !== -1 &&
+    getPositionCost(newPaths[indexNew][0].slice(0, posNew + 1)) >=
+      getPositionCost(currPath) + points
+  ) {
+    newPaths.splice(indexNew, 1);
+    isShorter = true;
+  }
+  return isShorter;
+}
+
+function changeMap(x: string, y: string, char: string) {
+  const newLine = grid[y].split("");
+  newLine[x] = char;
+  grid[y] = newLine.join("");
 }
 
 async function readfile(): Promise<string[]> {
-  const file = Bun.file("test.txt");
+  const file = Bun.file("input.txt");
   return (await file.text()).split("\n");
 }
 
